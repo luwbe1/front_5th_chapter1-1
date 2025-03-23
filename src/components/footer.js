c097092233a6ce8418cd841aeaa1225faca78bc4
yubin-kim <luwbe1@naver.com>
2025-03-24 01:41:48 +0900

    Design: footer.js components에 분리

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,4 @@
-export const Footer = () => `
+export const Footer = () => /*html*/ `
         <footer class="bg-gray-200 p-4 text-center">
           <p>&copy; 2024 항해플러스. All rights reserved.</p>
         </footer>
