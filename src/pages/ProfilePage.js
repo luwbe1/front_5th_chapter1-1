0957fcb66578c798bf78913c875b53dc3fcf3626
luwbe1@naver.com <luwbe1@naver.com>
2025-03-27 22:48:24 +0900

    Fix: 절대 경로 사용

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,5 +1,5 @@
-import Footer from "../components/Footer.js";
-import Header from "../components/Header.js";
+import Footer from "/src/components/Footer.js";
+import Header from "/src/components/Header.js";
 
 const ProfilePage = () => {
   const user = JSON.parse(localStorage.getItem("user"));
